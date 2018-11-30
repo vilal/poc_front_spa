5b6b4dc5674e434bca2536fd996236d8a483abe2
Virginie <v.lalande@studio-hb.com>
2018-11-30 16:29:17 +0100

    ADD [bootstrap vue] Install bootstrap vue & config.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
+import './config'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './stylesheets/main.sass'
 
-Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
