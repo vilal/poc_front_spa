d204945e4ab33d1e891cc71ac6b5b4846ed3dff3
Virginie <v.lalande@studio-hb.com>
2019-01-04 15:46:40 +0100

    UP Using the /deep/ vue loader selector

--- src/components/Transitions/fadeUpAccommodations.vue
+++ src/components/Transitions/fadeUpAccommodations.vue
@@ -3,7 +3,7 @@
     slot
 </template>
 
-<style lang="sass">
+<style lang="sass" scoped>
 
 $easings: (default: cubic-bezier(.4, .25, .3, 1), in: cubic-bezier(.6, .1, .8, .7), out: cubic-bezier(.2, .3, .4, .9), in-out: cubic-bezier(.5, .15, .4, 1), bounce: cubic-bezier(.4, .25, .3, 1.4))
 
@@ -11,21 +11,22 @@ $easings: (default: cubic-bezier(.4, .25, .3, 1), in: cubic-bezier(.6, .1, .8, .
 @function cb($fn: default) 
   @return map-get($easings, $fn)
 
-.accommodation__feed-move
-  transition: all 325ms cb()
-.accommodation__feed-enter-active
-  transition: all 325ms cb(out) 175ms
-.accommodation__feed-appear-enter-active
-  transition: all 325ms cb(out) 200ms
+/deep/
+  .accommodation__feed-move
+    transition: all 325ms cb()
+  .accommodation__feed-enter-active
+    transition: all 325ms cb(out) 175ms
+  .accommodation__feed-appear-enter-active
+    transition: all 325ms cb(out) 200ms
 
-.accommodation__feed-leave-active
-  transition: all 325ms cb(in)
-  position: absolute
-  z-index: -1
+  .accommodation__feed-leave-active
+    transition: all 325ms cb(in)
+    position: absolute
+    z-index: -1
 
-.accommodation__feed-enter,
-.accommodation__feed-leave-to,
-.accommodation__feed-appear-enter
-  opacity: 0
-  transform: translate3d(0, -2.5rem, 0)
+  .accommodation__feed-enter,
+  .accommodation__feed-leave-to,
+  .accommodation__feed-appear-enter
+    opacity: 0
+    transform: translate3d(0, -2.5rem, 0)
 </style>
