1c82d5cefe01c2928868a14e38ae93e1467ad3d7
Virginie <v.lalande@studio-hb.com>
2019-01-04 19:01:01 +0100

    WIP

--- src/components/AccommodationCard.vue
+++ src/components/AccommodationCard.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-
-
   b-card(
     class="shadow border-0 overflow-hidden"
     no-body
+    @mouseenter="handleEnter"
+    @mouseleave="handleLeave"
   )
     b-link(:to="{name: 'accommodation', params: { id: accommodation.id }}")
       progressive-img(
@@ -11,9 +11,11 @@
         :placeholder="accommodation.featuredImagePlaceholder"
         :aspect-ratio="0.68"
         class="img-cover-wrapper"
+        ref="featuredImage"
+        @onLoadPlaceholder="setBoundingRect"
       )
       p.z-index-md.position-absolute.pin-l.pin-t.pin-r.pl-3.pt-2.pb-2.text-uppercase.text-sm.linear-gradient.text-white {{ accommodation.contract_type }}
-    b-card-body(class="p-3")
+    b-card-body(class="p-3" )
       .d-flex.flex-column.justify-content-between.h-100
         div
           h2.mb-0.text-sm {{ accommodation.title }}
@@ -24,6 +26,10 @@
 
         p.text-primary {{ accommodation.price | currency }}
 
+    div(v-show="isOver" ref="clonedImage" class="transition-all")
+      b-link(:to="{name: 'accommodation', params: { id: accommodation.id }}")
+        img( :src="accommodation.featuredImage" class="img-cover" )
+
 </template>
 
 <script>
@@ -33,14 +39,77 @@
         type: Object,
         default: () => []
       }
+    },
+
+    beforeDestroy() {
+      this.handleEnter()
+    },
+
+    data() {
+      return {
+        isOver: false,
+        boudingRect: {}
+      }
+    },
+
+    methods: {
+      setBoundingRect() {
+        const element = this.$refs.featuredImage.$el
+        const { top, left, width, height } = element.getBoundingClientRect()
+        const scrollTop =
+          window.pageYOffset ||
+          document.documentElement.scrollTop ||
+          document.body.scrollTop ||
+          0
+        const scrollLeft =
+          window.pageXOffset ||
+          document.documentElement.scrollLeft ||
+          document.body.scrollLeft ||
+          0
+
+        this.boudingRect = {
+          top: top + scrollTop,
+          left: left + scrollLeft,
+          width,
+          height
+        }
+      },
+
+      handleEnter () {
+        this.isOver = true
+
+        let { top, left, width, height } = this.boudingRect
+
+        let el = this.$refs.clonedImage
+
+        el.style.position = "fixed"
+        el.style.top = `${top}px`
+        el.style.left = `${left}px`
+        el.style.height = `${height}px`
+        el.style.width = `${width}px`
+        el.style.zIndex = `100`
+
+        setTimeout(() => {
+          el.classList.add('transitioned')
+          el.style.width = `${window.innerWidth}px`
+          el.style.height = `${window.innerHeight}px`
+        }, 125)
+      },
+
+      handleLeave() {
+        this.isOver = false
+        this.$refs.clonedImage.classList.remove('transitioned')
+      }
     }
   }
 </script>
 
 
 <style lang="sass" scoped>
+.transition-all
+  transition: all 325ms
 
+.transitioned
+  top: 20px!important
+  left: 0!important
 </style>
-
-
-
--- src/views/accommodations/Show.vue
+++ src/views/accommodations/Show.vue
@@ -1,3 +1,39 @@
 <template lang="pug">
-  h1 Hello
+  div
+    progressive-img(
+      :src="accommodation.featuredImage"
+      :placeholder="accommodation.featuredImagePlaceholder"
+      :aspect-ratio="0.68"
+      class="img-cover-wrapper"
+    )
+
+    .container
+      h1 {{ accommodation.title }}
+
+      div {{ accommodation.description }}
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      accommodation: {
+        id: 1,
+        title: 'Maison',
+        price: 10000,
+        city: 'Lyon',
+        contract_type: 'Offre en cours',
+        tags: [
+          'piscine',
+          'belle vue'
+        ],
+        featuredImage: 'https://source.unsplash.com/random/1920x1080',
+        featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108',
+
+        description: "Charbonnières, dans secteur résidentiel à 1 km du village, maison de plain-pied de 219 m2 dans magnifique parc paysager et boisé de 5072 m2 au calme absolu et sans vis à vis. Elle comprend au rez-de-chaussée un hall d’entrée, triple espace de vie très lumineux avec salon coin TV de 44m2 avec baies vitrées ouvertes sur spacieuse terrasse de 200 m2 exposée plein sud, salle à manger avec cheminée, espace Lecture ou Bridge, cuisine équipée avec coin repas, cellier, buanderie, 4 chambres avec placards dont suite parentale, salle de bains, 2 sanitaires. Sous-sol complet avec atelier, bureau de 31 m2, local technique et pièce de stockage, garage double. Piscine en mosaïque à filtration sable équipée d’un rideau. Cabane de Jardin. Maison parfaitement entretenue à deux pas du parc Lacroix- Laval et de la Gare (accès St Paul), à proximité des ramassages pour Blaise Pascal et St Joseph. Possibilité d’agrandissement."
+      }
+    }
+  }
+}
+</script>
