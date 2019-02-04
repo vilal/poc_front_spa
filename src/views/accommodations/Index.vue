b1db384bee19cac71fc23785ddefd07c5fe69e5f
vilal <lalandev@free.fr>
2019-02-04 17:35:53 +0100

    ADD [accommodations data] separate json + axios

--- src/views/accommodations/Index.vue
+++ src/views/accommodations/Index.vue
@@ -5,98 +5,31 @@
 
 <script>
 import AccommodationList from "../../components/AccommodationList"
+import axios from 'axios'
 
 export default {
   components: {
     AccommodationList
   },
   mounted() {
-      let interval
-      interval = setInterval(() => {
-        this.accommodations.push(this.fakeDate.shift())
 
-        if (!this.fakeDate.length) {
-          clearInterval(interval)
+    axios
+      .get('/accommodations.json')
+      .then( response => {
+        let interval
+        interval = setInterval(() => {
+          this.accommodations.push(response.data.shift())
+
+          if (!response.data.length) {
+            clearInterval(interval)
+          }
+        }, 125)
         }
-      }, 125)
+      )
   },
 
   data () {
     return {
-      fakeDate: [
-        {
-          id: 0,
-          title: 'Maison 0',
-          price: 10000,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [
-            'piscine',
-            'belle vue'
-          ],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        },
-        {
-          id: 1,
-          title: 'coucou 1',
-          price: 12,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [
-            'piscine avec debordement de la mort qui tue',
-            'belle vue'
-          ],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        },
-        {
-          id: 2,
-          title: 'Maison 2',
-          price: 10000,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        },
-        {
-          id: 3,
-          title: 'Maison 3',
-          price: 10000,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [
-            'piscine',
-            'belle vue'
-          ],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        },
-        {
-          id: 4,
-          title: 'coucou 4',
-          price: 12,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [
-            'piscine avec debordement de la mort qui tue',
-            'belle vue'
-          ],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        },
-        {
-          id: 5,
-          title: 'Maison 5',
-          price: 10000,
-          city: 'Lyon',
-          contract_type: 'Offre en cours',
-          tags: [],
-          featuredImage: 'https://source.unsplash.com/random/1920x1080',
-          featuredImagePlaceholder: 'https://source.unsplash.com/random/192x108'
-        }
-      ],
       accommodations: []
     }
   }
